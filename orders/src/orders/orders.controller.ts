6b03d7a455a941be213ddb0bb1624b65b7ea4cdc
libterty <albert@ohbottech.com>
2020-03-01 21:23:32 +0800

    adding getOrderById & getOrderByUserId & updateOrderById & deleteOrderById feature

--- orders/src/orders/orders.controller.ts
+++ orders/src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, request } from 'express';
 import {
   Controller,
   Get,
@@ -8,6 +8,10 @@ import {
   Param,
   ParseIntPipe,
   Body,
+  UnauthorizedException,
+  Put,
+  Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -21,10 +25,36 @@ export class OrdersController {
     private readonly authService: AuthService,
   ) {}
 
-  getOrder(token: string): Promise<object> {
+  private isAuth(token: string): Promise<object> {
     return new Promise<object>(resolve => {
       resolve(this.authService.validate(token));
     });
+  }
+
+  @Get('/:id')
+  async getOrderById(
+    @Req() request: Request,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<object> {
+    const auth = await this.isAuth(request.headers.authorization);
+    if (auth) {
+      return this.orderService.getOrderById(id);
+    } else {
+      throw new UnauthorizedException();
+    }
+  }
+
+  @Get('/users/:userId')
+  async getOrderByUserId(
+    @Req() request: Request,
+    @Param('userId', ParseIntPipe) userId: number,
+  ): Promise<object> {
+    const auth = await this.isAuth(request.headers.authorization);
+    if (auth) {
+      return this.orderService.getOrderByUserId(userId);
+    } else {
+      throw new UnauthorizedException();
+    }
   }
 
   @Post('/:userId/:bookId')
@@ -33,10 +63,40 @@ export class OrdersController {
     @Param('userId', ParseIntPipe) userId: number,
     @Param('bookId', ParseIntPipe) bookId: number,
     @Body() ordersCreateDto: OrdersCreateDto,
-  ) {
-    const result = await this.getOrder(request.headers.authorization);
-    if (result) {
+  ): Promise<object> {
+    const auth = await this.isAuth(request.headers.authorization);
+    if (auth) {
       return this.orderService.createOrder(userId, bookId, ordersCreateDto);
+    } else {
+      throw new UnauthorizedException();
+    }
+  }
+
+  @Put('/:id')
+  async updateOrderById(
+    @Req() request: Request,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() ordersCreateDto: OrdersCreateDto,
+  ): Promise<object> {
+    const auth = await this.isAuth(request.headers.authorization);
+    if (auth) {
+      return this.orderService.updateOrderById(id, ordersCreateDto);
+    } else {
+      throw new UnauthorizedException();
+    }
+  }
+
+  @Delete('/:id')
+  @HttpCode(204)
+  async deleteOrderById(
+    @Req() request: Request,
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<void> {
+    const auth = await this.isAuth(request.headers.authorization);
+    if (auth) {
+      return this.orderService.deleteOrderById(id);
+    } else {
+      throw new UnauthorizedException();
     }
   }
 }
--- orders/src/orders/orders.service.ts
+++ orders/src/orders/orders.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderRepository } from './orders.repository';
 import { OrdersCreateDto } from './dto/orders.dto';
+import { Not } from 'typeorm';
 
 @Injectable()
 export class OrdersService {
@@ -9,9 +10,26 @@ export class OrdersService {
     @InjectRepository(OrderRepository) private orderRepository: OrderRepository,
   ) {}
 
+  async getOrderById(id: number): Promise<object> {
+    const order = await this.orderRepository.getOrderById(id);
+    if (!order) {
+      throw new NotFoundException(`Order with Id: ${id} cannot found`);
+    }
+    return { statusCode: '200', order };
+  }
+
+  async getOrderByUserId(userId: number): Promise<object> {
+    console.log('userid log:', userId);
+    const orders = await this.orderRepository.getOrderByUserId(userId);
+    if (!orders) {
+      throw new NotFoundException(`Order with UserId: ${userId} cannot found`);
+    }
+    return { statusCode: '200', orders };
+  }
+
   async createOrder(
-    userId,
-    bookId,
+    userId: number,
+    bookId: number,
     ordersCreateDto: OrdersCreateDto,
   ): Promise<object> {
     const order = await this.orderRepository.createOrder(
@@ -20,8 +38,31 @@ export class OrdersService {
       ordersCreateDto,
     );
     if (!order) {
-      throw new NotFoundException(`Order with Id: ${bookId} not create`);
+      throw new NotFoundException(`Order with Book Id: ${bookId} not create`);
     }
     return { statusCode: '201', order };
+  }
+
+  async updateOrderById(
+    id: number,
+    ordersCreateDto: OrdersCreateDto,
+  ): Promise<object> {
+    const order = await this.orderRepository.updateOrderById(
+      id,
+      ordersCreateDto,
+    );
+
+    if (!order) {
+      throw new NotFoundException(`Order with Id: ${id} cannot found`);
+    }
+    return { statusCode: '200', order };
+  }
+
+  async deleteOrderById(id: number): Promise<void> {
+    const result = await this.orderRepository.delete({ id });
+    if (result.affected === 0) {
+      throw new NotFoundException(`Order with Id: ${id} cannot found`);
+    }
+    return;
   }
 }
