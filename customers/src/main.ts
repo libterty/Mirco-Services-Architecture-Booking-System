f37a1611c47a2a1d103dce49c2bebb71a460fa55
libterty <albert@ohbottech.com>
2020-03-15 15:57:32 +0800

    add multi dockerfile and micro

--- customers/src/main.ts
+++ customers/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: true });
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: { retryAttempts: 5, retryDelay: 1000 },
+  });
   const port = process.env.PORT || 4546;
   await app.listen(port);
 }
